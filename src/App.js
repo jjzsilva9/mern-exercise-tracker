f4f7e3e9226b28db0bb5eaf8f4e5b3a65a9fbf18
jjzsilva9 <jjzsilva9@gmail.com>
2023-01-26 22:26:25 +0000

    create react components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-//import { Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<ExercisesList />} />
+        <Route path="/edit/:id" element={<EditExercise />} />
+        <Route path="/create" element={<CreateExercise />} />
+        <Route path="/user" element={<CreateUser />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
